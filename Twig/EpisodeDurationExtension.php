a0ad866ebef07b4a68e3a8c1a97423ed03a2b598
rs <schmidt91robin@icloud.com>
2018-06-22 11:44:28 +0200

    simplified media index, episode duration won't show 0 hours anymore

--- Twig/EpisodeDurationExtension.php
+++ Twig/EpisodeDurationExtension.php
@@ -16,7 +16,10 @@ class EpisodeDurationExtension extends \Twig_Extension
         $minutes = floor(($duration - $hours*3600)/60);
         $seconds = ($duration - $hours*3600 - $minutes*60);
 
-        return sprintf("%s:%s:%s", $hours, $minutes, $seconds);
+        if ($hours) {
+            return sprintf("%s:%s:%s", $hours, $minutes, $seconds);
+        }
+        return sprintf("%s:%s", $minutes, $seconds);
     }
 
     public function getName() {
